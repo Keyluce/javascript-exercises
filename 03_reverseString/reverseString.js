10c97b82c846d16798dc2a169b8b7f44ffe1e4dd
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-12 22:49:01 +0530

    Do exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arrayOfString = string.split("");
+    start = 0;
+    end = arrayOfString.length - 1;
 
+    while (start < end)
+    {
+        let temporary;
+        temporary = arrayOfString[start];
+        arrayOfString[start] = arrayOfString[end];
+        arrayOfString[end] = temporary;
+        start++;
+        end--;
+    }
+    string = arrayOfString.join("");
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
