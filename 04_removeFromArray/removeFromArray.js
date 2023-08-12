466331f673ab795c63c18d5c764ee8387e2079c6
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-13 00:13:18 +0530

    Do exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeThese) {
+    for (const removeThis of removeThese) {
+        let index = array.indexOf(removeThis);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
