21e777f597077540b6ae78c8920521fa7bf7f501
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-24 21:08:09 +0530

    Do exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.split('').map(char => char.toLowerCase())
+    .filter(char => ((char.codePointAt(0) > 96 && char.codePointAt(0) < 123) 
+    || (char.codePointAt(0) > 47 && char.codePointAt(0) < 58))).reverse().join('') == string.split('').map(char => char.toLowerCase())
+    .filter(char => ((char.codePointAt(0) > 96 && char.codePointAt(0) < 123) 
+    || (char.codePointAt(0) > 47 && char.codePointAt(0) < 58))).join('');
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
 
+    for (let i = 0; i < num - 2; i++)
+    {
+        let next = num1 + num2;
+        [num1,num2] = [num2,next];
+    }
+    return next;
 };
 
 // Do not edit below this line
