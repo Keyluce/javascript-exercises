cbe9e0813002f04e6f8cf48a1a95c9bcfb6e3ed3
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-24 21:20:27 +0530

    Do exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,12 @@
 const fibonacci = function(num) {
-    let num1 = 0;
+    let num1 = 1;
     let num2 = 1;
-
+    if (num < 0) return 'OOPS';
     for (let i = 0; i < num - 2; i++)
     {
-        let next = num1 + num2;
-        [num1,num2] = [num2,next];
+        [num1,num2] = [num2,num1 + num2];
     }
-    return next;
+    return num2;
 };
 
 // Do not edit below this line
