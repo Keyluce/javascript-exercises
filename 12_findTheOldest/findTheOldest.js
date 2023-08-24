fcecfd168b1b55450c41b42ae696dfcee36fe840
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-24 22:48:46 +0530

    Do 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((totalObj, obj) => {
+        let age;
+        if (obj.yearOfDeath == undefined) {
+            let year = new Date().getFullYear();
+            age = year - obj.yearOfBirth;
+        } 
+        else age = obj.yearOfDeath - obj.yearOfBirth;
+        
+        if (totalObj.oldest < age)
+        {
+            totalObj.oldest = age;
+            totalObj.name = obj.name;
+        }
+        return totalObj;
+        
+    },{name: '', oldest: 0})
 };
 
 // Do not edit below this line
